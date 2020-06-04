5436e8cbd76b2395a7614b96ca654357591342b2
Abhishek Tangod <43072302+abhishekvtangod@users.noreply.github.com>
2020-06-04 16:12:59 +0530

    Initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+    // as setState() is async we need callback for console log
+  />
+);
